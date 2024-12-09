a0ec247208d8ca68f7ca0aae61ec445c3daf322e
sreenaadhlanka <22h51a05p7@cmrcet.ac.in>
2024-12-09 15:08:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.get('/' , (req , res) => {
+    res.send("Hello World");
+});
+app.listen(3000 , () =>{
+    console.log("Server in {PORT}");
+});
